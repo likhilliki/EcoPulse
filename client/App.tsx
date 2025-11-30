ea3f2518019db7d8a23e1b1597113f998ea50ff4
Likhil Gowda K A <likhilgowda89@gmail.com>
2025-11-30 14:27:06 +0530

    Add files via upload

---
+++ client/App.tsx
@@ -0,0 +1,39 @@
+import { Switch, Route } from "wouter";
+import { queryClient } from "./lib/queryClient";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { Toaster } from "@/components/ui/toaster";
+import { TooltipProvider } from "@/components/ui/tooltip";
+import NotFound from "@/pages/not-found";
+import Home from "@/pages/home";
+import Dashboard from "@/pages/dashboard";
+import MapPage from "@/pages/map";
+import Login from "@/pages/login";
+import Signup from "@/pages/signup";
+
+function Router() {
+  const token = localStorage.getItem("authToken");
+  
+  return (
+    <Switch>
+      <Route path="/" component={Home} />
+      <Route path="/login" component={Login} />
+      <Route path="/signup" component={Signup} />
+      <Route path="/dashboard" component={token ? Dashboard : Login} />
+      <Route path="/map" component={token ? MapPage : Login} />
+      <Route component={NotFound} />
+    </Switch>
+  );
+}
+
+function App() {
+  return (
+    <QueryClientProvider client={queryClient}>
+      <TooltipProvider>
+        <Router />
+        <Toaster />
+      </TooltipProvider>
+    </QueryClientProvider>
+  );
+}
+
+export default App;
